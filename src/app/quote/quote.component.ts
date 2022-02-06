cf995c1103f276dc4264e4ec28eb4bc27545e362
Mercy <mercycherotich757@gmail.com>
2022-02-06 20:38:54 +0300

    updates

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -14,9 +14,7 @@ export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
    new Quote('The greatest glory in living lies not in never falling, but in rising every time we fall. ','Nelson Mandela','Mercy Bore',new Date(2020,3,13)),
    new Quote('The way to get started is to quit talking and begin doing.','Walt Disney','Mercy Bore',new Date(2020,3,13)),
-   new Quote('If life were predictable it would cease to be life, and be without flavor.','Eleanor Roosevelt','Mercy Bore',new Date(2020,3,13)),
-   new Quote('Life is what happens when you are busy making other plans.','John Lennon','Mercy Bore',new Date(2020,3,13))
-  ];
+   ];
 
   toggleAuthor(index: any){
     this.quotes[index].showAuthor = !this.quotes[index].showAuthor;
